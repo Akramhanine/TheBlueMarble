ef0d98169d732b04dea70b4e1bd579a8dc1c2a95
Matt <mslagle@matthewslagle.com>
2013-04-20 11:10:55 -0400

    Updating test harness with geocoding

--- BlueMarble.TestHarness/Program.cs
+++ BlueMarble.TestHarness/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using GeoCoding;
 
 namespace BlueMarble.TestHarness
 {
@@ -9,6 +10,12 @@ namespace BlueMarble.TestHarness
     {
         static void Main(string[] args)
         {
+            // Testing geocoding utilities
+            Address address = GeocodingUtils.MicrosoftGeocodeAddress("28500 Clemens Road Westlake, OH 44145");
+            Console.WriteLine("Found latitude and longtiude for {0}", address.FormattedAddress);
+            Console.WriteLine("Latitude, Longitude:  {0}, {1}", address.Coordinates.Latitude, address.Coordinates.Longitude);
+
+            Console.ReadLine();
         }
     }
 }
