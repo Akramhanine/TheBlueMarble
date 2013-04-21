fcf8a471ff7105bacee1e4f8f785c484c58d87fd
Jeramiah Cieszynski <jcieszynski@gmail.com>
2013-04-20 20:07:18 -0400

    Added GetImagesbyFeature API method

--- BlueMarble.Website/APIControllers/FeatureController.cs
+++ BlueMarble.Website/APIControllers/FeatureController.cs
@@ -33,5 +33,37 @@ namespace BlueMarble.Website.APIControllers
 
 			return features;
 		}
+
+		/// <summary>
+		/// Returns all image data for a given feature.
+		/// Since the feature column of the data given is not delimited properly, this method will do a slower "like" search
+		/// </summary>
+		/// <param name="location">feature</param>
+		/// <returns>IEnumerable list of ImageData.</returns>
+		public IEnumerable<ImageData> GetImagesByFeature(string feature)
+		{
+			//TODO: make this a more efficient and more concise query instead of three queries
+
+			string featureUpper = feature.ToUpper();
+
+			//use feature name to get a list of feature IDs
+			//featureID is unique, but multiple feature names may exist as one name in the featuredesc table
+			//due to poorly constructed non-delimited data in the input dataset
+			IEnumerable<int> featureIDs = from feat in Database.Featuredesc
+										  where featureUpper.IndexOf(feat.Name) >= 0
+										  select feat.FeaturedescID;
+
+			//use list of feature IDs to get a list of image IDs
+			IEnumerable<int> imageIDs = from imagexfeature in Database.Imagexfeature
+										where featureIDs.Contains((int)imagexfeature.FeaturedescID)
+										select imagexfeature.ImageDataID;
+
+			//use list of image IDs to get a list of ImageData
+			IEnumerable<ImageData> data = from image in Database.Imagedata
+										  where imageIDs.Contains((int)image.ImageDataID)
+										  select image;
+
+			return data;
+		}
     }
 }
