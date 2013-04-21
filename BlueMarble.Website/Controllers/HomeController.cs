b51203dfd7f2d93ca58b4766105ed56d9a191050
Ryan Wilson <link1305@gmail.com>
2013-04-21 02:09:41 -0400

    Forgot a file

--- BlueMarble.Website/Controllers/HomeController.cs
+++ BlueMarble.Website/Controllers/HomeController.cs
@@ -44,7 +44,8 @@ namespace BlueMarble.Website.Controllers
             if (!page.HasValue || _searchImagesData == null)
             {
                 var client = new HttpClient();
-				client.BaseAddress = new Uri("http://bigmarble.azurewebsites.net/");
+				client.BaseAddress = new Uri("http://localhost:2245"); // Uncomment for local testing
+				//client.BaseAddress = new Uri("http://bigmarble.azurewebsites.net/"); // Uncomment for azure publishing
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 HttpResponseMessage response = client.GetAsync("api/image?address=" + Address).Result;  // Blocking call!
                 if (response.IsSuccessStatusCode)
