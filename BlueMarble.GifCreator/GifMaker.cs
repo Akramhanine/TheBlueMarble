bc29b27d17568fbb6dd99a644f37986284e454a5
Dan V <selth20@gmail.com>
2013-04-21 11:30:35 -0400

    Gif final
    added comments and a default value for the create method

--- BlueMarble.GifCreator/GifMaker.cs
+++ BlueMarble.GifCreator/GifMaker.cs
@@ -11,14 +11,19 @@ namespace BlueMarble.GifCreator
 {
     public class GifMaker
     {
-        public GifBitmapEncoder Create(IEnumerable<BlueMarble.Data.ImageData> listOFImages, int frameRate)
+
+        //This method takes in a list of imagedata found in the database, creates a new gifbitmapencoder and puts the images into the newly formed gif.
+        public GifBitmapEncoder Create(IEnumerable<BlueMarble.Data.ImageData> listOFImages, int frameRate = 2)
         {
             JpegBitmapDecoder decoder;
             BitmapSource convertToBmp;
             GifBitmapEncoder newGif = new GifBitmapEncoder();
 
+
+            //Run through the list of ImageData from the database
             foreach (BlueMarble.Data.ImageData indImage in listOFImages)
             {
+                //Get the image based off of the url found on the data of the image
                 var request = WebRequest.Create(indImage.Lowresurl);
 
                 using (var response = request.GetResponse())
@@ -27,6 +32,7 @@ namespace BlueMarble.GifCreator
                     decoder = new JpegBitmapDecoder(webstream, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
                     convertToBmp = decoder.Frames[0];
 
+                    //The amount of times you store the image in the in the gif, the slower it appears to transition, default for this speed is 2
                     for (int x = 0; x < frameRate; x++)
                     {
                         newGif.Frames.Add(BitmapFrame.Create(convertToBmp));
@@ -36,35 +42,6 @@ namespace BlueMarble.GifCreator
             }
 
             return newGif;
-
-            /*FileStream stream = new FileStream("new.gif", FileMode.Create);
-
-
-
-            //Stream imageStream = new FileStream("images.jpg", FileMode.Open, FileAccess.Read, FileShare.Read);
-
-
-            JpegBitmapDecoder decoder = new JpegBitmapDecoder(imageStream, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
-            BitmapSource image = decoder.Frames[0];
-            GifBitmapEncoder newGif = new GifBitmapEncoder();
-
-
-            var request = WebRequest.Create("http://www.gravatar.com/avatar/6810d91caff032b202c50701dd3af745?d=identicon&r=PG");
-
-            using (var response = request.GetResponse())
-            using (var webstream = response.GetResponseStream())
-            {
-                decoder = new JpegBitmapDecoder(webstream, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
-                //imageStream = Bitmap.FromStream(webstream);
-            }
-            //decoder = new JpegBitmapDecoder(imageStream, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
-            image = decoder.Frames[0];
-            for (int x = 0; x < 10; x++)
-            {
-                newGif.Frames.Add(BitmapFrame.Create(image));
-            }
-
-            newGif.Save(stream);*/
         }
     }
 }
