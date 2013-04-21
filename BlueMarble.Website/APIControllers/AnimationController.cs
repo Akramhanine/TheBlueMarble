4cb785868084009e2bc643f09e3a8debf4e48675
Jeramiah Cieszynski <jcieszynski@gmail.com>
2013-04-21 12:55:52 -0400

    intermediate check in 2

--- BlueMarble.Website/APIControllers/AnimationController.cs
+++ BlueMarble.Website/APIControllers/AnimationController.cs
@@ -11,10 +11,12 @@ namespace BlueMarble.Website.APIControllers
     public class AnimationController : ApiController
     {
 		MarbleDataBase _database;
+		int _gifImageRange;
 
 		public AnimationController()
 		{
 			_database = new MarbleDataBase();
+			_gifImageRange = 10; //defaulting to 10
 		}
 
 		public MarbleDataBase Database
@@ -39,23 +41,63 @@ namespace BlueMarble.Website.APIControllers
 		}
 
 		/// <summary>
-		/// Returns an animated gif by combining the low resolution images in the given list of image IDs
+		/// Returns a list of images before and after the given image based on the passed in range.
+		/// Images returned are from the same dataset and roll as the base image.
 		/// </summary>
-		/// <param name="rollNum">roll number</param>
-		/// <returns>Gif</returns>
-		//public GetAnimationLowRes()
-		//{
-				//call GetImageSeries
-		//}
+		/// <param name="imageStart">starting image</param>
+		/// <param name="range">range of images to create series</param>
+		/// <returns></returns>
+		public IEnumerable<ImageData> GetImageSeries(ImageData imageStart, int range)
+		{
+			//use dataset ID and roll to query imagedata for additional images in a series
+			IEnumerable<ImageData> data = from image in Database.Imagedata
+										  where (image.DatasetID == imageStart.DatasetID &&
+												image.Rollnum == imageStart.Rollnum && 
+												image.Framenum >= 0)
+										  select image;
+
+			return data;
+		}
+
+		/// <summary>
+		/// Generates and returns a link to an animated gif by combining the low resolution images.
+		/// The list of images is generated from a single image ID by combining images from the same roll.
+		/// Returns an empty string if gif generation fails.
+		/// </summary>
+		/// <param name="rollNum">imageID</param>
+		/// <returns>url to animated gif hosted on the server</returns>
+		public string GetAnimationLowRes(int imageID)
+		{
+			string url = "";
+
+			//get imagedata for imageID
+			ImageController imageController = new ImageController();
+			ImageData image = imageController.GetImageByID(imageID);
+			
+			//get a series of images based on the start image and range
+			GetImageSeries(image, _gifImageRange);
+
+			//pass images into gif animation generator
+
+
+			return url;
+		}
 
 		/// <summary>
 		/// Returns an animated gif by combining the high resolution images in the given list of image IDs
+		/// NOTE: some images do not have high resolution versions.
 		/// </summary>
 		/// <param name="rollNum">roll number</param>
 		/// <returns>Gif</returns>
 		//public GetAnimationHighRes()
 		//{
 				//call GetImageSeries
+		//}
+
+
+		//private string GenerateGifFromImageSeries()
+		//{
+
 		//}
     }
 }
