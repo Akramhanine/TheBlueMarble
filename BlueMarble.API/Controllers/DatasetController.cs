a403ebc069761bdea12148d051fe23abaf64eb1a
Ryan Wilson <link1305@gmail.com>
2013-04-22 23:12:40 -0400

    Moved the web API into BlueMarble.API and reorganized the geocoding projects/folders

---
+++ BlueMarble.API/Controllers/DatasetController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BlueMarble.Data;
+
+namespace BlueMarble.Website.APIControllers
+{
+    public class DatasetController : ApiController
+    {
+		MarbleDataBase _database;
+
+		public DatasetController()
+		{
+			_database = new MarbleDataBase();
+		}
+
+		public MarbleDataBase Database
+		{
+			get { return _database; }
+		}
+
+		/// <summary>
+		/// Returns the dataset for a given mission
+		/// </summary>
+		/// <param name="mission">mission</param>
+		/// <returns>Dataset</returns>
+		public Dataset GetDatasetByMission(string setMission)
+		{
+			string missionUpper = setMission.ToUpper();
+
+			Dataset dataset = (from data in Database.Dataset
+							  where data.Description == missionUpper
+							  select data).First();
+
+			return dataset;
+		}
+
+		/// <summary>
+		/// Returns all images for a given mission
+		/// </summary>
+		/// <param name="mission">mission</param>
+		/// <returns>IEnumerable list of ImageData</returns>
+		public IEnumerable<ImageData> GetImagesByMission(string mission)
+		{
+			Dataset dataset = GetDatasetByMission(mission);
+
+			//use list of image IDs to get a list of ImageData
+			IEnumerable<ImageData> data = from image in Database.Imagedata
+										  where image.DatasetID == dataset.DatasetID
+										  select image;
+
+			return data;
+		}
+
+		/// <summary>
+		/// Returns a list of all rolls in a given mission
+		/// </summary>
+		/// <param name="mission">mission</param>
+		/// <returns>IEnumerable list of roll IDs</returns>
+		public IEnumerable<int> GetRollsByMission(string rollsMission)
+		{
+			Dataset dataset = GetDatasetByMission(rollsMission);
+
+			IEnumerable<int> rolls =  (from image in Database.Imagedata
+										where image.DatasetID == dataset.DatasetID
+										select image.Rollnum).Distinct();
+
+			return rolls;
+		}
+
+
+	}
+}
---
+++ BlueMarble.API/Controllers/ImageController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BlueMarble.Data;
+using GeoCoding;
+
+namespace BlueMarble.Website.APIControllers
+{
+    public class ImageController : ApiController
+    {
+		MarbleDataBase _database;
+		double _defaultRange; // default range of miles used when searching for images using a street address
+
+		public ImageController()
+		{
+			_database = new MarbleDataBase();
+			_defaultRange = 100; //defaulting to 100 miles
+		}
+
+		public MarbleDataBase Database
+		{
+			get { return _database; }
+		}
+
+		/// <summary>
+		/// Returns all image data for given image ID
+		/// </summary>
+		/// <param name="id">Image ID</param>
+		/// <returns>ImageData object</returns>
+        public ImageData GetImageByID(int id)
+        {
+			ImageData data = Database.Imagedata.Find(id);
+			if (data == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+			return data;
+        }
+
+		/// <summary>
+		/// Returns all image data for a given street address
+		/// Uses the default range to search, with the street address as the center point.
+		/// </summary>
+		/// <param name="address">street address</param>
+		/// <returns>IEnumerable list of ImageData</returns>
+		public IEnumerable<ImageData> GetImagesByAddress(string address)
+		{
+			Address addressConverted = GeocodingUtils.MicrosoftGeocodeAddress(address);
+			CoordinateRange coords = GeocodingUtils.GetLongLatRangeByDistance(addressConverted.Coordinates, _defaultRange);
+
+			IEnumerable<ImageData> data = from image in Database.Imagedata
+										   where (image.Latitude > coords.LatitudeMin && image.Longitude > coords.LongitudeMin) &&
+												 (image.Latitude < coords.LatitudeMax && image.Longitude < coords.LongitudeMax)
+										   select image;
+			return data;
+		}
+
+		/// <summary>
+		/// Returns all image data for a given street address
+		/// Accepts a range of miles to search, with the street address as the center point.
+		/// </summary>
+		/// <param name="address">street address</param>
+		/// <returns>IEnumerable list of ImageData.</returns>
+		public IEnumerable<ImageData> GetImagesByAddress(string address, double range)
+		{
+			Address addressConverted = GeocodingUtils.MicrosoftGeocodeAddress(address);
+			CoordinateRange coords = GeocodingUtils.GetLongLatRangeByDistance(addressConverted.Coordinates, range);
+
+			IEnumerable<ImageData> data = from image in Database.Imagedata
+										   where (image.Latitude > coords.LatitudeMin && image.Longitude > coords.LongitudeMin) &&
+												 (image.Latitude < coords.LatitudeMax && image.Longitude < coords.LongitudeMax)
+										   select image;
+			return data;
+		}
+
+
+    }
+}
